c4147bb0216e00f290f22a729ed88529875a0fe8
TOWHIDpro <sajjad.rk05@gmail.com>
2022-04-30 16:58:45 +0600

    1st commit

---
+++ components/tryfetch.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        Page visits: {{ data[1].website }}
+    </div>
+</template>
+<script setup lang="ts">
+    const { data } = await useAsyncData('count', () => $fetch('http://127.0.0.1:8000/api/view/platform'))
+</script>
+<style lang="">
+    
+</style>
