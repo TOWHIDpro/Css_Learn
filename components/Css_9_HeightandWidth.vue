0e1938d6157c0ad78e700e6d4dff2cf5011ae4a1
TOWHIDpro <sajjad.rk05@gmail.com>
2022-05-30 17:22:05 +0600

    css_7 added

---
+++ components/Css_9_HeightandWidth.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="hightandwidth">
+    <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus sapiente qui molestias ad ea facere facilis repudiandae amet quis nam</p>
+    <p class="text2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus sapiente qui molestias ad ea facere facilis repudiandae amet quis nam</p>
+    <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus sapiente qui molestias ad ea facere facilis repudiandae amet quis nam</p>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+p{
+    border: 2px solid black;
+}
+.text2{
+    width: 200px;
+}
+</style>z
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,18 @@
 <template>
 <div>
+    <!-- <Css_07_Backgrounds /> -->
+    <Css_9_HeightandWidth />
     <!-- <Css_27_table /> -->
-    <Css_28_functions />
+    <!-- <Css_28_functions /> -->
+
 </div>
 </template>
 
 <script setup>
 import Css_27_table from "../components/css_27_table.vue";
 import Css_28_functions from "../components/css_28_functions.vue";
+import Css_07_Backgrounds from "~~/components/Css_07_Backgrounds.vue";
+import Css_9_HeightandWidth from "~~/components/Css_9_HeightandWidth.vue";
 
 
 </script>
